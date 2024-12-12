b8fa5c388425e994ead4f9f902f5ccd74fd81b77
tiagolimar <tiagolima.r2@gmail.com>
2024-12-12 20:23:38 -0300

    update README.md

--- README.md
+++ README.md
@@ -14,27 +14,87 @@ Essa estrutura já está dockerizada, então basta ter o docker compose rodando 
 ## Como usar
 
 Passo 1: Executar o docker
-
 `docker compose up -d`
 
 Passo 2: Entre no container
-
 `docker exec -it setup-laravel_php bash`
 
 Passo 3: E execute o composer install
-
 `composer install`
 
 Passo 4: Crie uma arquivo .env a partir de .env.example
-
 `cp .env.example .env`
 
 Passo 5: Você também precisa gerar a chave da aplicação
-
 `php artisan key:generate`
 
 Passo 6: Aplique as migrações
-
 `php artisan migrate`
 
 Passo 7: Pronto, agora acesse o http://localhost:8080
+
+## Estrutura inicial das classes/tabelas
+
+Ver esquema representativo das [tabelas](https://dbdesigner.page.link/rdiaJVDmpVa2Agvf8)
+
+```mermaid
+classDiagram
+    class User {
+        +id: int
+        +full_name: string
+        +image: string
+        +user_name: string
+        +created_at: datetime
+        +updated_at: datetime
+        +deleted_at: datetime
+    }
+
+    class Board {
+        +id: int
+        +name: string
+        +order: int
+        +color: string
+        +created_at: datetime
+        +updated_at: datetime
+        +deleted_at: datetime
+    }
+
+    class Card {
+        +id: int
+        +priority_type_id: int
+        +title: string
+        +order: int
+        +description: string
+        +user_id: int
+        +board_id: int
+        +created_at: datetime
+        +updated_at: datetime
+        +deleted_at: datetime
+    }
+
+    class Comment {
+        +id: int
+        +description: string
+        +user_id: int
+        +card_id: int
+        +created_at: datetime
+        +updated_at: datetime
+        +deleted_at: datetime
+    }
+
+    class PriorityType {
+        +id: int
+        +name: string
+        +color: string
+        +created_at: datetime
+        +updated_at: datetime
+        +deleted_at: datetime
+    }
+
+    User "1" --> "many" Board : Owns
+    Board "1" --> "many" Card : Contains
+    Card "1" --> "many" Comment : Has
+    Card "1" --> "1" PriorityType : Uses
+    Card "many" --> "1" User : AssignedTo
+```
+
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,17 +13,34 @@
         <section class="card rounded rounded-4 shadow-lg w-[16em] max-w-[30em] border-t-2 border-blue-600 bg-stone-100">
             <header class="flex align-center justify-between py-1 px-2 bg-stone-300">
                 <h3 class="text-xl">Pendências</h3>
-                <p class="text-2xl">+</p>
-            </header>
-            <article>
-                <div class="bg-white rounded shadow m-4 p-2">
-                    <p class="bg-red-500 rounded-full px-2 py-1 text-sm text-white font-semibold">#PRIORIDADE</p>
-                    <h3 class="py-3">Título da tarefa a ser executada</h3>
-                    <footer class="flex justify-between align-center">
-                        <p class="text-sm py-1">Comentários: 0</p>
-                        <img class="rounded-full w-[2em]" src="https://avatars.githubusercontent.com/u/47084105?v=4" alt="" srcset="">
-                    </footer>
+                <div class="buttons flex gap-1">
+                    <p class="cursor-pointer hover:text-white hover:bg-stone-400 text-2xl bg-stone-200 rounded-full w-6 text-center h-6 text-stone-700 flex justify-center">-</p>
+                    <p class="cursor-pointer hover:text-white hover:bg-stone-400 text-2xl bg-stone-200 rounded-full w-6 text-center h-6 text-stone-700 flex justify-center">+</p>
                 </div>
+            </header>
+            <article class="bg-white rounded shadow m-4 p-2 hover:bg-stone-200 cursor-pointer">
+                <p class="bg-red-500 rounded-full px-2 py-1 text-sm text-white font-semibold">#PRIORIDADE</p>
+                <h3 class="py-3">Título da tarefa a ser executada</h3>
+                <footer class="flex justify-between align-center">
+                    <p class="text-sm py-1">Comentários: 0</p>
+                    <img class="rounded-full w-[2em]" src="https://avatars.githubusercontent.com/u/47084105?v=4" alt="" srcset="">
+                </footer>
+            </article>
+            <article class="bg-white rounded shadow m-4 p-2 hover:bg-stone-200 cursor-pointer">
+                <p class="bg-red-500 rounded-full px-2 py-1 text-sm text-white font-semibold">#PRIORIDADE</p>
+                <h3 class="py-3">Título da tarefa a ser executada</h3>
+                <footer class="flex justify-between align-center">
+                    <p class="text-sm py-1">Comentários: 0</p>
+                    <img class="rounded-full w-[2em]" src="https://avatars.githubusercontent.com/u/47084105?v=4" alt="" srcset="">
+                </footer>
+            </article>
+            <article class="bg-white rounded shadow m-4 p-2 hover:bg-stone-200 cursor-pointer">
+                <p class="bg-red-500 rounded-full px-2 py-1 text-sm text-white font-semibold">#PRIORIDADE</p>
+                <h3 class="py-3">Título da tarefa a ser executada</h3>
+                <footer class="flex justify-between align-center">
+                    <p class="text-sm py-1">Comentários: 0</p>
+                    <img class="rounded-full w-[2em]" src="https://avatars.githubusercontent.com/u/47084105?v=4" alt="" srcset="">
+                </footer>
             </article>
         </section>
     </main>
